7e087449148e7538cb634816df2a22a0dd640c53
ProNintendo101 <davidgorozco@outlook.com>
2023-09-06 12:32:23 -0400

    Time to do something

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void onMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+}
